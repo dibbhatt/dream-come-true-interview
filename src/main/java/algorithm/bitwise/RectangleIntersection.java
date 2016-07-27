8df2068c431e2d3c27397582a6a5cb1b3fb22d7a
xmpy <xiaomengzhaopy@gmail.com>
2016-07-27 17:24:59 +0800

    ddd

---
+++ src/main/java/algorithm/bitwise/RectangleIntersection.java
@@ -0,0 +1,43 @@
+package algorithm.bitwise;
+
+public class RectangleIntersection {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        // Left 
+        if (A <= E) {
+            // Up
+            if (D >= H) {
+                // Not intersect
+                if (E >= C || H <= B) {
+                    return 0;
+                }
+                return Math.abs((Math.min(C, G) - E) * ( H - Math.max(B, F)));
+            } else  { // Down
+                if (E >= C || D <= F) {
+                    return 0;
+                }
+                return Math.abs((E - Math.min(C, G)) * ( D- Math.max(B, F)));
+            }
+        } else { // Right
+            // Up
+            if (D >= H) {
+                // Not intersect
+                if (E >= C || H <= B) {
+                    return 0;
+                }
+                return Math.abs((Math.min(C, G) - E) * ( H - Math.max(B, F)));
+            } else  { //Right
+                if (D >= H) { // Up
+                    if (A >= G || B >= H) {
+                      return 0;
+                    }
+                    return Math.abs((A - Math.min(C, G)) * ( H - Math.max(B, F)));
+                } else  { // Down
+                    if (A >= G || D <= F) {
+                        return 0;
+                    }
+                    return Math.abs((A - Math.min(C, G)) * ( D- Math.max(B, F)));
+                }
+            }      
+        }
+    }
+}
---
+++ src/test/java/algorithm/bitwise/RectangleAreaTest.java
@@ -0,0 +1,13 @@
+package algorithm.bitwise;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RectangleAreaTest {
+	@Test
+	public void testComputeArea() {
+		RectangleArea ra = new RectangleArea();
+		assertEquals( 16, ra.computeArea(-2, -2, 2, 2, -4, -2, -2, 2));
+		assertEquals( 19, ra.computeArea(-2, -2, 2, 2, -3, -3, -1, -1));
+	}
+}
