1ac052f727f2505d0ed1e03c3667960cb0bd4462
zhaoxm <xiaomengzhaopy@gmail.com>
2016-08-04 22:38:32 +0800

    Add multiply strings

---
+++ src/test/java/algorithm/array/MultiplyStringsTest.java
@@ -0,0 +1,16 @@
+package algorithm.array;
+
+import org.junit.Test;
+
+public class MultiplyStringsTest {
+	@Test
+	public void testMultiply() {
+		MultiplyStrings ms = new MultiplyStrings();
+		//System.out.println(ms.multiply("12", "11"));
+		System.out.println(ms.multiply("9", "9"));
+
+		
+	}
+	
+	
+}
