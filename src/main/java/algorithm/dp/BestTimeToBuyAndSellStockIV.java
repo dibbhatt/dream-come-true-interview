23d3991ec121534af69575c4bf1bfb63f9bdf0f8
zhaoxm <xiaomengzhaopy@gmail.com>
2016-08-05 00:14:31 +0800

    Add best time to buy and sell iv

--- src/main/java/algorithm/dp/BestTimeToBuyAndSellStockIV.java
+++ src/main/java/algorithm/dp/BestTimeToBuyAndSellStockIV.java
@@ -52,13 +52,35 @@ public class BestTimeToBuyAndSellStockIV {
              locals[i][0] = 0;
          }
          for (int i = 1; i <= prices.length; i++) {
-             for (int j = 1; j <= k; j++) {
-                 int diff = (i == 1) ? 0 : (prices[i - 1] - prices[i - 2]);
+             int diff = (i == 1) ? 0 : (prices[i - 1] - prices[i - 2]);
+        	 for (int j = 1; j <= k; j++) {
             	 locals[i][j] = Math.max(locals[i - 1][j] + diff, states[i - 1][j - 1] + diff);
                  states[i][j] = Math.max(locals[i][j], states[i - 1][j]);   
              }
          }
          
          return states[prices.length][k];
+    }
+    
+    public int maxProfitSaveSpace(int k, int[] prices) {
+        if ( k >= (prices.length / 2)) {
+            return maxProfitNoLimitedK(prices);
+         }
+         
+         int[]  states = new int[k + 1];
+         int[]  locals = new int[k + 1];
+
+         Arrays.fill(states, 0);
+         Arrays.fill(locals, 0);
+
+         for (int i = 1; i <= prices.length; i++) {
+             int diff = (i == 1) ? 0 : (prices[i - 1] - prices[i - 2]);
+        	 for (int j = k; j >= 0; j--) {
+            	 locals[j] = Math.max(locals[j] + diff, states[j - 1] + diff);
+                 states[j] = Math.max(locals[j], states[j]);   
+             }
+         }
+         
+         return states[k];
     }
 }
