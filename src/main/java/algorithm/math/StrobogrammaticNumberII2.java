bdc8c72b1a864b94fd96ccfe3db81cd9499f8021
zhaoxm <xiaomengzhaopy@gmail.com>
2016-08-08 12:36:21 +0800

    modify

---
+++ src/main/java/algorithm/math/StrobogrammaticNumberII2.java
@@ -0,0 +1,12 @@
+package algorithm.math;
+
+import java.util.List;
+
+public class StrobogrammaticNumberII2 {
+    public List<String> findStrobogrammatic(int n) {
+    	String[] n1 = new String[]{"0", "1", "8"};
+    	String[] n2 = new String[]{"00", "11", "88", ""}
+    	
+    
+    }
+}
